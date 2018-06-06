3155904a1642ca3a988cb962263274f641775183
Yusuf Tezel <yusuftezel@Yusufs-MacBook-Pro.local>
2018-06-07 00:15:48 +0200

    Topbar animation, light status bar, amount label. ADDED

--- guya/ViewController.swift
+++ guya/ViewController.swift
@@ -11,14 +11,31 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    var isCategoryMenuOpen = false
+    
     var topBar: UIView = {
         let v = UIView()
         v.backgroundColor = .red
         return v
     }()
     
+    var amountLabel: UILabel = {
+        let l = UILabel()
+        l.font = UIFont.systemFont(ofSize: 30.0)
+        l.textColor = .white
+        l.text = "12930 kr"
+        return l
+    }()
+    
+    var topBarExtension: UIView = {
+        let v = UIView()
+        v.backgroundColor = .red
+        return v
+    }()
+    
     var plusButton: UIButton = {
-       let b = UIButton()
+        let b = UIButton()
+        b.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
         b.setTitle("+", for: .normal)
         b.titleLabel?.font = b.titleLabel?.font.withSize(40)
         return b
@@ -28,8 +45,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(topBar)
+        view.addSubview(topBarExtension)
         topBar.addSubview(plusButton)
-        
+        topBar.addSubview(amountLabel)
+        snpControls()
+    }
+    
+    @objc func plusButtonTapped(sender: UIButton){
+        self.isCategoryMenuOpen = !self.isCategoryMenuOpen
+        if(self.isCategoryMenuOpen){
+            topBarExtension.snp.updateConstraints {
+                $0.height.equalTo(100)
+            }
+            UIView.animate(withDuration: 0.25) {
+                self.plusButton.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/4)
+                self.view.layoutIfNeeded()
+            }
+        }else {
+            topBarExtension.snp.updateConstraints {
+                $0.height.equalTo(0)
+            }
+            UIView.animate(withDuration: 0.25, animations: {
+                self.plusButton.transform = .identity
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    func snpControls(){
         plusButton.snp.makeConstraints {
             $0.trailingMargin.equalToSuperview().offset(-20)
             $0.bottomMargin.equalToSuperview()
@@ -39,7 +82,19 @@ class ViewController: UIViewController {
             $0.leadingMargin.equalTo(view.snp.leading)
             $0.trailingMargin.equalTo(view.snp.trailing)
             $0.topMargin.equalTo(view.snp.top)
-            $0.height.equalTo((view.bounds.height / 5))
+            $0.height.equalTo((view.bounds.height / 6))
+        }
+        
+        amountLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-10)
+        }
+        
+        topBarExtension.snp.makeConstraints {
+            $0.leadingMargin.equalTo(view.snp.leading)
+            $0.trailingMargin.equalTo(view.snp.trailing)
+            $0.topMargin.equalTo(topBar.snp.bottom)
+            $0.height.equalTo(0)
         }
     }
 }
